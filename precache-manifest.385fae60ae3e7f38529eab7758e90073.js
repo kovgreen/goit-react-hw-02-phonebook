f122f5c5d06ae68517e39b0a0189a774c0a311db
kovgreen <kovgreen@gmail.com>
2020-06-17 12:52:17 +0300

    Updates

---
+++ precache-manifest.385fae60ae3e7f38529eab7758e90073.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a6d66f577aa888ededc1b4e24c14327",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "6abd213083f0cb0f3ef0",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.9ec3e329.chunk.css"
+  },
+  {
+    "revision": "acc2d0d1436cc624a883",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.468bcd66.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.468bcd66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6abd213083f0cb0f3ef0",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.9ba957f9.chunk.js"
+  },
+  {
+    "revision": "de5dfbc78302bed1e7de",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.3cc9cabd.js"
+  }
+]);
